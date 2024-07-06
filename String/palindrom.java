cce414cbbeac6129efb9a3ab18ab27bf4f2df2b7
ANISH KANRAR <147317580+Anish-Kanrar@users.noreply.github.com>
2024-07-06 23:29:39 +0530

    Update palindrom.java

--- String/palindrom.java
+++ String/palindrom.java
@@ -3,7 +3,23 @@ import java.util.*;
 //Check if a String is a Palindrome.
 //example-->"racecar","noon","madam";
 public class palindrom {
-    public static void main(String args[]){
+    public static boolean isPalindrome(String str) {
+        for (int i = 0; i < str.length() / 2; i++) {
+            int n = str.length();
+            if (str.charAt(i) != str.charAt(n - 1 - i)) {
+                // not a palindrome
+                return false;
+            }
+        }
+        return true;
+    }
 
+    public static void main(String args[]) {
+        String str = "racecar";
+
+        System.out.println(isPalindrome(str));
     }
 }
+
+// true/false
+// time complexity->O(n)
