d12d62b39138add8715d91cc1508a0f441488400
AK47 <147317580+Anish-Kanrar@users.noreply.github.com>
2025-03-15 23:43:20 +0530

    Update linkedList.java

--- LInkedList/linkedList.java
+++ LInkedList/linkedList.java
@@ -128,7 +128,31 @@ public class linkedList {
         }
         head=prev;
     }
+    public void deleteNthfromEnd(int n){
 
+        //calculate size
+        int sz = 0;
+        Node temp = head;
+        while(temp != null){
+            temp=temp.next;
+            sz++;
+        }
+        if(n == sz){
+            head=head.next; //remove first
+            return;
+        }
+
+        //sz-n
+        int i=1;
+        int iToFind = sz-n;
+        Node prev = head;
+        while(i <iToFind){
+            prev = prev.next;
+            i++;
+        }
+        prev.next=prev.next.next;
+        return;
+    }
     public void print(){ //O(n)
         if(head == null){
             System.out.println("LL is empty");
